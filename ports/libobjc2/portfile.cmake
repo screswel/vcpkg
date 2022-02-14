c2faecf2c00bf54ae41a4f51aed9d87dd480071d
Stephen Creswell <stephen.creswell@btinternet.com>
2022-02-14 21:11:13 +0000

    Added libobjc2 port.

---
+++ ports/libobjc2/portfile.cmake
@@ -0,0 +1,54 @@
+# TODO - should I be using $ENV{VCPKG_ROOT} ?
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CURRENT_BUILDTREES_DIR}/../../scripts/toolchains/windows-llvm.cmake")
+
+# XXX - needed to avoid some link.exe errors
+set(VCPKG_POLICY_SKIP_ARCHITECTURE_CHECK enabled)
+set(VCPKG_POLICY_SKIP_DUMPBIN_CHECKS enabled)
+
+# XXX - setup Visual Studio environment - even though VCPKG_CHAINLOAD_TOOLCHAIN_FILE is set
+set(VCPKG_LOAD_VCVARS_ENV ON)
+
+# XXX - ensure clang-cl can treat all objc/objc++ files as c/c++ files
+set(ENV{CCC_OVERRIDE_OPTIONS} "x-TC x-TP x/TC x/TP")
+
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
+
+find_program(GIT git)
+
+set(GIT_URL "https://github.com/gnustep/libobjc2.git")
+set(GIT_REF 2.1)
+
+if(NOT EXISTS "${SOURCE_PATH}/.git")
+    # message(STATUS "Cloning and fetching submodules")
+    vcpkg_execute_required_process(
+        COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
+        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+        LOGNAME clone
+    )
+
+    # message(STATUS "Checkout")
+    vcpkg_execute_required_process(
+        COMMAND ${GIT} checkout v${GIT_REF}
+        WORKING_DIRECTORY ${SOURCE_PATH}
+        LOGNAME checkout
+    )
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
+file(RENAME ${CURRENT_PACKAGES_DIR}/lib/objc.dll ${CURRENT_PACKAGES_DIR}/bin/objc.dll)
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/objc.dll ${CURRENT_PACKAGES_DIR}/debug/bin/objc.dll)
+
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+vcpkg_fixup_pkgconfig()
